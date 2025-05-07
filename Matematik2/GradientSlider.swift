929892fc3a5a2e59e980386a6c6786653eae0fb6
Sebastian Strus <sebastianstrus@Sebastians-MacBook-Air.local>
2025-05-07 16:06:08 +0200

    Updated slider

--- Matematik2/GradientSlider.swift
+++ Matematik2/GradientSlider.swift
@@ -11,18 +11,28 @@ struct GradientSlider: View {
     @Binding var value: Int
     var range: ClosedRange<Int>
     var step: Int
-
+    @Environment(\.layoutDirection) var layoutDirection
+    
+    @State private var initialValue: Int = 0
+    @State private var dragStartOffset: CGFloat = 0
+    
     var body: some View {
         GeometryReader { geometry in
-            let sliderWidth = geometry.size.width
-
+            let sliderWidth = geometry.size.width - 30 // Account for thumb width
+            let currentOffset = calculateThumbOffset(sliderWidth: sliderWidth)
+            
             ZStack(alignment: .leading) {
-                LinearGradient(colors: [.blue, .purple, .purple], startPoint: .leading, endPoint: .trailing)
-                    .frame(height: 4)
-                    .cornerRadius(2)
-                    .padding(.leading, 15)
-                    .padding(.trailing, -15)
-
+                // Track with gradient
+                LinearGradient(
+                    colors: [.blue, .purple, .purple],
+                    startPoint: layoutDirection == .rightToLeft ? .trailing : .leading,
+                    endPoint: layoutDirection == .rightToLeft ? .leading : .trailing
+                )
+                .frame(height: 4)
+                .cornerRadius(2)
+                .padding(.horizontal, 15)
+                
+                // Thumb with value display
                 Circle()
                     .frame(width: 30, height: 30)
                     .foregroundColor(.white)
@@ -30,22 +40,40 @@ struct GradientSlider: View {
                     .overlay(
                         Text("\(value)")
                             .foregroundColor(.black)
-                            .font(Font.system(size: 10))
+                            .font(.system(size: 10))
                     )
-                    .offset(x: CGFloat(Double(value - range.lowerBound) / Double(range.upperBound - range.lowerBound)) * (sliderWidth - 30))
-                    
+                    .offset(x: currentOffset)
                     .gesture(
-                        DragGesture()
+                        DragGesture(minimumDistance: 0)
                             .onChanged { gesture in
-                                let location = gesture.location.x
-                                let newValue = Double(location / (sliderWidth - 30)) * Double(range.upperBound - range.lowerBound) + Double(range.lowerBound)
-                                let roundedValue = Int(round(newValue / Double(step))) * step
-                                value = min(max(range.lowerBound, roundedValue), range.upperBound)
+                                if dragStartOffset == 0 {
+                                    initialValue = value
+                                    dragStartOffset = gesture.startLocation.x - 15 // Account for padding
+                                }
+                                
+                                let dragDistance = gesture.translation.width
+                                let adjustedDistance = layoutDirection == .rightToLeft ? -dragDistance : dragDistance
+                                let percentageChange = adjustedDistance / sliderWidth
+                                
+                                let valueRange = Double(range.upperBound - range.lowerBound)
+                                let newValue = Double(initialValue) + (percentageChange * valueRange)
+                                let steppedValue = Int(round(newValue / Double(step))) * step
+                                value = min(max(range.lowerBound, steppedValue), range.upperBound)
+                            }
+                            .onEnded { _ in
+                                dragStartOffset = 0
+                                initialValue = 0
                             }
                     )
             }
             .frame(height: 44)
-            .padding(.horizontal, 15)
-        }.frame(height: 44)
+            .contentShape(Rectangle())
+        }
+        .frame(height: 44)
+    }
+    
+    private func calculateThumbOffset(sliderWidth: CGFloat) -> CGFloat {
+        let percentage = CGFloat(value - range.lowerBound) / CGFloat(range.upperBound - range.lowerBound)
+        return percentage * sliderWidth
     }
 }
